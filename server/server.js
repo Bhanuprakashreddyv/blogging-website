c1b989e75760764d461908ad6f265c92a2c2c29a
Bhanuprakashreddyv <v.bhanureddy184@gmail.com>
2024-03-20 09:29:19 +0000

    Add Jwt for hashing

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 import bcrypt from 'bcrypt';
 import { nanoid } from 'nanoid';
+import jwt from 'jsonwebtoken';
 
 //Schemas 
 import User from './Schema/User.js';
@@ -20,7 +21,10 @@ mongoose.connect(process.env.DB_LOCATION, {
 })
 
 const formatDatatoSend = (user) => {
+
+    const access_token = jwt.sign({ id: user._id }, process.env.SECRET_ACESS_KEY)
     return {
+        access_token,
         profile_img: user.personal_info.profile_img,
         username: user.personal_info.username,
         fullname: user.personal_info.fullname
@@ -70,10 +74,35 @@ server.post("/signup", (req, res) => {
             return res.status(500).json({ "error": err.message })
         })
     })
-
-    return res.status(200).json({ "status": "okay" })
 })
 
-// server.listen(PORT, () => {
-//     console.log('listening on port . ' + PORT)
-// })
+server.post("/signin", (req, res) => {
+    let {  email, password } = req.body;
+
+    User.findOne({ "personal_info.email": email })
+    .then((user) => {
+        if(!user) {
+            return res.status(403).json({ "error": "Email not found" });
+        }
+        
+        bcrypt.compare(password, user.personal_info.password, (err, result) => {
+            if(err) {
+                return res.status(403).json({ "error": "Error occured while login please try again" });
+            }
+
+            if(!result){
+                return res.status(403).json({ "error": "Incorrect Password"})
+            } else{
+                return res.status(200).json(formatDatatoSend(user))
+            }
+        })
+    })
+    .catch(err => {
+        console.log(err.message);
+        return res.status(500).json({ "error": err.message })
+    })
+})
+
+server.listen(PORT, () => {
+    console.log('listening on port . ' + PORT)
+})
