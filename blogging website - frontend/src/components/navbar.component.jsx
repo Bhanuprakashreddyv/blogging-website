af155997c165355b2b52eb29cc4b5f1c997fa4ce
Bhanuprakashreddyv <v.bhanureddy184@gmail.com>
2024-03-20 15:53:36 +0000

    Add Signout functionalities.

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,12 +1,16 @@
 import { Link,Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import logo from "../imgs/logo.png";
+import { UserContext } from "../App";
+import UserNavigationPanel from "./user-navigation.component";
 
 
 
 const Navbar = () => {
 
     const[ searchBoxVisibility, setSearchBoxVisilibity ] = useState(false)
+
+    const { userAuth, userAuth: { access_token, profile_img } } = useContext(UserContext)
 
     return (
         <>
@@ -43,12 +47,35 @@ const Navbar = () => {
                     <p>Writer</p>
                 </Link>
 
-                <Link className="btn-dark py-2" to="/signin" >
-                    Sign In
-                </Link>
-                <Link className="btn-light py-2 hidden md:block" to="/signup" >
-                    Sign Up
-                </Link>
+                {
+                    access_token ?
+                    <>
+                        <Link to="/dashboard/notification">
+                            <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
+                                <i className="fi fi-rr-bell text-2xl block mt-1"></i>
+                            </button>
+                        </Link>
+
+                        <div className="relative">
+                            <button className="w-12 h-12 mt-1">
+                                <img src={profile_img} className="w-full h-full object-cover rounded-full"/>
+                            </button>
+
+                            <UserNavigationPanel />
+
+                        </div>
+                    </>
+                    :
+                    <>
+                        <Link className="btn-dark py-2" to="/signin" >
+                            Sign In
+                        </Link>
+                        <Link className="btn-light py-2 hidden md:block" to="/signup" >
+                            Sign Up
+                        </Link>
+                    </>
+                }
+
             </div>
 
 
